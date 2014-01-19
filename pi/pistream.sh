bcefa06a767fdd1af7286277ebb66c8ad7b39a69
Michael Proctor-Smith <mproctor13@gmail.com>
2014-01-19 02:16:55 -0800

    fix typeo

--- pi/pistream.sh
+++ pi/pistream.sh
@@ -2,4 +2,4 @@
 
 # this streams colorbars with static
 
-gst-launch-1.0 -v videotestsrc ! 'video/x-raw, width=640, height=480, framerate=15/1' ! queue ! videoconvert ! omxh264enc ! rtph264pay pt=96 ! udpsink host=#{1} port=5000
+gst-launch-1.0 -v videotestsrc ! 'video/x-raw, width=640, height=480, framerate=15/1' ! queue ! videoconvert ! omxh264enc ! rtph264pay pt=96 ! udpsink host=${1} port=5000
--- pi/vidstream.sh
+++ pi/vidstream.sh
@@ -2,5 +2,5 @@
 
 # stream h264 from /dev/video0 to hostip 
 
-gst-launch-1.0 -v v4l2src device=/dev/video0 norm=45056 ! omxh264enc !  rtph264pay pt=96 ! udpsink host=#{1} port=5000
+gst-launch-1.0 -v v4l2src device=/dev/video0 norm=45056 ! omxh264enc !  rtph264pay pt=96 ! udpsink host=${1} port=5000
 
